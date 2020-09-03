04a9fe23bf2242b027a13d8fbbf3eee6cd263599
Goodness Ezeokafor <gootech442@yahoo.com>
2020-09-03 08:49:44 +0100

    added

--- routes/home.js
+++ routes/home.js
@@ -7,4 +7,16 @@ router.get('/', (req,res)=>{
 })
 
 
+router.get('/about', (req,res)=>{
+    res.render("About.ejs")
+})
+
+router.get('/contact', (req,res)=>{
+    res.render("Contact.ejs")
+})
+
+// router.get('/', (req,res)=>{
+//     res.render("Home.ejs")
+// })
+
 module.exports = router
