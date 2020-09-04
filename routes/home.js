2fe3097d061d9dee0efd9137255acfe29edd2845
Goodness Ezeokafor <gootech442@yahoo.com>
2020-09-04 14:40:11 +0100

    fixed images

--- routes/home.js
+++ routes/home.js
@@ -1,6 +1,7 @@
 const router = require("express").Router()
 
 
+const request= require("request")
 
 router.get('/', (req,res)=>{
     res.render("Home.ejs")
@@ -13,6 +14,44 @@ router.get('/about', (req,res)=>{
 
 router.get('/contact', (req,res)=>{
     res.render("Contact.ejs")
+})
+
+router.post("/newsletter", async(req, res) => {
+    const {email} = req.body
+    
+    const data = {
+        members:[
+          {
+            email_address:req.body.email,
+            status:"subscribed",
+            // merge_fields:{
+            //     FNAME:username,
+            // }
+          }
+        ]
+      }
+      const postData = JSON.stringify(data) 
+      const options = {
+        url :"https://us19.api.mailchimp.com/3.0/lists/02e1d16e87",
+        method:'POST',
+        headers:{
+          Authorization:"auth API_KEY"
+        },
+        body:postData
+      };
+  
+      request(options, (err, response,body)=>{
+        if(err){
+          console.log("MAILCHIMP: ERROR", err)
+        } else{
+          if(response.statusCode === 200){
+            console.log("SUCCESS")
+          } else {
+            console.log("FAILED")
+          }
+        }
+      })
+      
 })
 
 // router.get('/', (req,res)=>{
